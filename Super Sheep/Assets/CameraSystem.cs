285817539bc519e846c60f5e858228ea7297e220
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-11-20 10:15:25 -0100

    Add camera script (not finished). Will implement character death demo

---
+++ Super Sheep/Assets/CameraSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour {
+
+	public GameObject player;
+	private float xMin;
+	private float xMax;
+	private float yMin;
+	private float yMax;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
