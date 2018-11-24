b3f2a78244e5a449724a752033dcb04781335301
Cactusforlife <Miguelsardinhapimentel@hotmail.com>
2018-11-24 19:38:02 -0100

    on clicking z, Distance X, Disspear bullet
    on clicking Z shoots bullet, if thunder hits asset, asset is not destroyed, after X time bullet is destroyd

---
+++ Super Sheep/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int health= 100;
+
+    public GameObject deathEffect;
+
+    public void TakeDamadge (int damadge)
+    {
+        health -= damadge;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+        
+    void Die()
+    {
+        
+        Destroy(gameObject);
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
