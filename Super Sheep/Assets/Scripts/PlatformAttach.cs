3ff98cedeb241354c032096999ed37edec3f4ed5
Paulo Cunha <paulo_cunha1997@outlook.pt>
2018-11-27 10:23:18 -0100

    Start to add cloud/plataform movement. Some level changes

---
+++ Super Sheep/Assets/Scripts/PlatformAttach.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformAttach : MonoBehaviour {
+
+    public GameObject Player;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == Player)
+            print("test");
+
+        {
+            Player.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+
+}
